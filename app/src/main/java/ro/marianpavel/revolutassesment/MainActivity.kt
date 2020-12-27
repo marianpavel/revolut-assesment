ca4bcb1598150c7c1bdb0a87c93f9b85aa6004ba
Marian <marianp.dot.com@gmail.com>
2020-12-27 13:49:35 +0200

    Initial commit

---
+++ app/src/main/java/ro/marianpavel/revolutassesment/MainActivity.kt
@@ -0,0 +1,11 @@
+package ro.marianpavel.revolutassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
