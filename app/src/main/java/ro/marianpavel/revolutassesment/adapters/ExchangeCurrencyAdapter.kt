2384d8586f17a7cbf97caa7308f61da64ce5ea4e
okarmazin <18285187+okarmazin@users.noreply.github.com>
2021-01-14 05:51:15 +0100

    Revert "Homework: replace Float by BigDecimal"
    It makes no sense now, I wrote that before converting
    amount to String.

--- app/src/main/java/ro/marianpavel/revolutassesment/adapters/ExchangeCurrencyAdapter.kt
+++ app/src/main/java/ro/marianpavel/revolutassesment/adapters/ExchangeCurrencyAdapter.kt
@@ -38,12 +38,6 @@ private class ExchangeDiffCallback : DiffUtil.ItemCallback<CurrencyViewModel>() 
         oldItem: CurrencyViewModel,
         newItem: CurrencyViewModel
     ): Boolean {
-        // TODO: Floats should not be tested for equality!
-        //       Replace all usages of Float by BigDecimal and compare by
-        //       decimal1.compareTo(decimal2) == 0 (equal mathematical value)
-        //                                    <  0 (decimal1 smaller)
-        //                                    >  0 (decimal1 bigger)
-        //       Do not use == to test BigDecimals for equality!
         return oldItem.amount == newItem.amount
     }
 
